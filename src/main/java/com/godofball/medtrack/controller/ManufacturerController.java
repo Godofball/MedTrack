0b1db286d4b0e2b58b520ebb9c53e318a44572dc
godofball <2070239066@qq.com>
2023-03-30 21:08:02 +0800

    MedTrack login,employee(page,add,update)

--- src/main/java/com/godofball/medtrack/controller/ManufacturerController.java
+++ src/main/java/com/godofball/medtrack/controller/ManufacturerController.java
@@ -1,6 +1,9 @@
 package com.godofball.medtrack.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.godofball.medtrack.common.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class ManufacturerController {
-
+  @GetMapping("/page")
+    public Result<Page> page(){
+      return null;
+  }
 }
