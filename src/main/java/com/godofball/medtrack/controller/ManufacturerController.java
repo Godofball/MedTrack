7abaf3df81b283ef18a5f06d1879d8999c454661
godofball <2070239066@qq.com>
2023-03-30 20:58:06 +0800

    MedTrack login,employee(page,add,update)

---
+++ src/main/java/com/godofball/medtrack/controller/ManufacturerController.java
@@ -0,0 +1,11 @@
+package com.godofball.medtrack.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/manufacturer")
+@RestController
+@Slf4j
+public class ManufacturerController {
+}
